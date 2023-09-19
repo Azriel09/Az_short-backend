2bbf2bcd3ba47b88c4038d978487c4da925b6b5e
unknown <jhanmarmel@gmail.com>
2023-09-19 19:08:57 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,126 @@
+const crypto = require('crypto')
+const cors = require('cors')
+const mysql = require("mysql")
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+require('dotenv').config()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    res.header("Access-Control-Allow-Credentials", true)
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+    next();
+  });
+  
+  app.use(cors());
+  app.use(express.json());
+  
+//   app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}
+
+const db = mysql.createConnection(dbConfig)
+
+
+db.connect((err) => {
+    if(err) {
+        console.log("Error connecting to MySQL RDS:", err)
+        return
+    }
+    console.log("Connected to MySQL RDS")
+
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS test (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      link VARCHAR(255) NOT NULL,
+      code VARCHAR(255) NOT NULL,
+      visited INT NOT NULL
+    )
+  `;
+  db.query(createTableQuery, (err, results) => {
+    if (err) {
+      console.error('Error creating table:', err);
+    } else {
+      console.log('Table created successfully');
+    }
+
+   
+    })
+
+   
+})
+
+app.get("/", (req, res) => {
+    res.json("Kore ga backend da!")
+})
+
+app.post("/submit",(req, res) => {
+    const q_insert = "INSERT INTO test (`link`, `code`, `visited`) VALUES (?)"
+    const q_find = "SELECT * FROM az_short.test WHERE shortened LIKE (?)"
+    let shortened = crypto.randomBytes(2).toString('hex')
+    let visited = 1
+    let values = [req.body.link, shortened, 1]
+
+    // const randomString = "SELECT SUBSTR(MD5(RAND()), 1, 10) AS randomString"
+    
+    const link = "192.168.137.1:8800/"
+    db.query(q_insert, [values], (err, data) => {
+            
+        if (err) {
+            res.json(err)
+        } else {
+            console.log("Link submitted")
+            return res.json(link+shortened)
+           
+        }
+    })
+
+})
+
+
+
+app.get("/:id", (res, req) => {
+    let value = res.params.id
+    let long_url = ''
+    
+    const q_find ="SELECT * FROM az_short.test WHERE code LIKE (?)"
+    db.query(q_find,[value],function(err, rows){
+        if (err) {
+            console.log(err)
+            req.redirect("https://az-short.netlify.app/error")
+        }
+        if (rows[0]){
+            
+            long_url = rows[0].link;
+            req.status(301)
+            if (long_url.includes("https://")){
+                req.redirect(long_url)
+            } else {
+                req.redirect("https://"+long_url)
+            }
+        } else {
+            console.log(rows)
+            req.redirect("https://az-short.netlify.app/error")
+        }
+    })
+  });
+
+
+app.listen(8800, () => {
+    console.log("Listening at port 8800")
+})
+
